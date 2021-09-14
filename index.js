cf3bb4453be37b0b317312f6455757503b15b351
Taufik Nurrohman <hi@taufik-nurrohman.com>
2021-09-14 08:34:55 +0700

    Refactor.

---
+++ index.js
@@ -0,0 +1,94 @@
+/*!
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright © 2021 Taufik Nurrohman <https://github.com/taufik-nurrohman>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the “Software”), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ */
+(function(global, factory) {
+    typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() : typeof define === 'function' && define.amd ? define(factory) : (global = typeof globalThis !== 'undefined' ? globalThis : global || self, global.K = factory());
+})(this, function() {
+    'use strict';
+    var isArray = function isArray(x) {
+        return Array.isArray(x);
+    };
+    var isDefined = function isDefined(x) {
+        return 'undefined' !== typeof x;
+    };
+    var isFunction = function isFunction(x) {
+        return 'function' === typeof x;
+    };
+    var isNull = function isNull(x) {
+        return null === x;
+    };
+    var isSet = function isSet(x) {
+        return isDefined(x) && !isNull(x);
+    };
+    var isString = function isString(x) {
+        return 'string' === typeof x;
+    };
+    var toObjectKeys = function toObjectKeys(x) {
+        return Object.keys(x);
+    };
+
+    function K(context = {}) {
+        let $ = this,
+            keys = {};
+        $.commands = {};
+        $.fire = command => {
+            let value, exist;
+            if (isFunction(command)) {
+                value = command.call(context);
+                exist = true;
+            } else if (isString(command) && (command = $.commands[command])) {
+                value = command.call(context);
+                exist = true;
+            } else if (isArray(command)) {
+                let data = command[1] || [];
+                if (command = $.commands[command[0]]) {
+                    value = command.apply(context, data);
+                    exist = true;
+                }
+            }
+            return null === value && exist ? true : value;
+        };
+        $.keys = {};
+        $.pull = key => {
+            if (!isSet(key)) {
+                return keys = {}, $;
+            }
+            return delete keys[key], $;
+        };
+        $.push = key => {
+            return keys[key] = 1, $;
+        };
+        $.test = () => {
+            let command = $.keys[$.toString()];
+            return isSet(command) ? command : false;
+        };
+        $.toString = () => {
+            return toObjectKeys(keys).join('-');
+        };
+        return $;
+    }
+    K.version = '2.0.0';
+    return K;
+});
---
+++ index.mjs
@@ -0,0 +1,57 @@
+import {isArray, isFunction, isSet, isString} from '@taufik-nurrohman/is';
+import {toObjectKeys} from '@taufik-nurrohman/to';
+
+function K(context = {}) {
+
+    let $ = this,
+        keys = {};
+
+    $.commands = {};
+
+    $.fire = command => {
+        let value, exist;
+        if (isFunction(command)) {
+            value = command.call(context);
+            exist = true;
+        } else if (isString(command) && (command = $.commands[command])) {
+            value = command.call(context);
+            exist = true;
+        } else if (isArray(command)) {
+            let data = command[1] || [];
+            if (command = $.commands[command[0]]) {
+                value = command.apply(context, data);
+                exist = true;
+            }
+        }
+        return null === value && exist ? true : value;
+    };
+
+    $.keys = {};
+
+    $.pull = key => {
+        if (!isSet(key)) {
+            return (keys = {}), $;
+        }
+        return (delete keys[key]), $;
+    };
+
+    $.push = key => {
+        return (keys[key] = 1), $;
+    };
+
+    $.test = () => {
+        let command = $.keys[$.toString()];
+        return isSet(command) ? command : false;
+    };
+
+    $.toString = () => {
+        return toObjectKeys(keys).join('-');
+    };
+
+    return $;
+
+}
+
+K.version = '2.0.0';
+
+export default K;
